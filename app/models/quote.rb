c7544f110e47e517a21fe2a37a331b1c135d11cb
muh-code-acct <skellytech.jg@gmail.com>
2019-09-06 08:57:24 +0000

    splurty

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord 
+    validates :saying, Presence: true, length: { maxinum: 140, minimum: 3}
+    validates :author, presence: true, length: { maximum: 50, minimum: 3}
 end
